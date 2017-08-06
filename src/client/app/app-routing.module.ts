0f1019d358b394794586c5b0634d4d1ef00ab73c
Wendy <honeykatie@qq.com>
2017-08-06 16:22:28 -0700

    Success to build and run for Chapter 2-4

--- src/client/app/app-routing.module.ts
+++ src/client/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { ContactListComponent } from './contact-list/contact-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: [],
-          redirectTo: 'contacts',
+    redirectTo: 'contacts',
     pathMatch: 'full'
   },
   {
