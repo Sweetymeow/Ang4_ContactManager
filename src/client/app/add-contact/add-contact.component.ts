3df53292b87fafeb26d701fc2febe0b554c4a8d4
Wendy <honeykatie@qq.com>
2017-08-06 23:41:34 -0700

    NgForm get error - 'standalone' in ngModelOptions.

--- src/client/app/add-contact/add-contact.component.ts
+++ src/client/app/add-contact/add-contact.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { NgForm } from '@angular/forms';
+import { Contact } from '../shared/contact.model';
 
 @Component({
   selector: 'app-add-contact',
   templateUrl: './add-contact.component.html',
   styleUrls: ['./add-contact.component.scss']
 })
-export class AddContactComponent implements OnInit {
 
-  constructor() { }
+export class AddContactComponent implements OnInit {
+  loading: Boolean = false;
+  newContact: Contact;
+
+  constructor(public http: Http) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    this.loading = true;
+    // Make hard copy of form values
+    const formValues = Object.assign({}, form.value);
+
+    // construct the new contact that would be added.
+    const contact: Contact = {
+      name: `${formValues.firstName} ${formValues.lastName}`,
+      address: formValues.address,
+      phone: `${formValues.areaCode} ${formValues.prefix}-${formValues.lineNumber}`,
+      photoUrl: formValues.photo
+    };
+
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    const requestOptions = new RequestOptions({ headers: headers });
+
+    this.http.post('/api/contacts', contact, requestOptions)
+      .map((res:Response) => res.json())
+      .subscribe(data => {
+        form.reset();
+        this.loading = false;
+        this.newContact = data;
+      })
   }
 
 }
